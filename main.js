224ae4c9c20f2917540f78380adc4e8836428d39
Vitiin044 <91144148+Vitoor044@users.noreply.github.com>
2024-05-15 08:43:17 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+
+contadores[0].textContent = tempoObjetivo1;
